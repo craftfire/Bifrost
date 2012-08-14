0fc5a753f07f5087227dca5d0ed420673637ff1a
Contex <contexmoh@gmail.com>
2012-08-14 09:03:50 +0200

    Import JTable in SMF.

--- src/main/java/com/craftfire/bifrost/scripts/forum/SMF.java
+++ src/main/java/com/craftfire/bifrost/scripts/forum/SMF.java
@@ -24,6 +24,7 @@ import com.craftfire.commons.CraftCommons;
 import com.craftfire.commons.enums.Encryption;
 import com.craftfire.commons.managers.DataManager;
 
+import javax.swing.*;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
