d3139e94fd28b52e60b728f3e880987885bfec83
Contex <contexmoh@gmail.com>
2013-03-31 15:58:59 +0200

    We weren't selecting from a table, just a prefix.

--- src/main/java/com/craftfire/bifrost/scripts/forum/PhpBB.java
+++ src/main/java/com/craftfire/bifrost/scripts/forum/PhpBB.java
@@ -1,22 +1,22 @@
 /*
-* This file is part of Bifrost.
-*
-* Copyright (c) 2011-2012, CraftFire <http://www.craftfire.com/>
-* Bifrost is licensed under the GNU Lesser General Public License.
-*
-* Bifrost is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Bifrost is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of Bifrost.
+ *
+ * Copyright (c) 2011-2012, CraftFire <http://www.craftfire.com/>
+ * Bifrost is licensed under the GNU Lesser General Public License.
+ *
+ * Bifrost is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bifrost is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.craftfire.bifrost.scripts.forum;
 
 import java.sql.SQLException;
@@ -99,7 +99,7 @@ public class PhpBB extends ForumScript {
     @Override
     public ForumUser getUser(int userid) throws SQLException {
         Results results = getDataManager().getResults("SELECT * FROM `" + getDataManager().getPrefix() +
-                                                      "` WHERE `user_id` = " + userid);
+                                                      "users` WHERE `user_id` = " + userid);
         DataRow row = results.getFirstResult();
         ForumUser user = new ForumUser(this,
                                        row.getIntField("user_id"),
